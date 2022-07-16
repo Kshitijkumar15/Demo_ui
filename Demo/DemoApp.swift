3c427f2fbb5170c60147344c0fca29f8fab1dc17
Kshitij <kshitijkumarsingh58@gmail.com>
2022-07-16 08:38:27 +0530

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by kshitij on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
